826081d8d5a280f8aa9de55b13b91684501b7da9
jack ovsiannikov <jack ovsiannikov>
2017-06-26 16:07:35 +0300

    add new pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
+	var mainSlider = $('#main-slider');
 	var slider = $('#slider');
 
+	mainSlider.on('init', function() {
+		setTimeout(function() {
+			$('.img-scale').imageScale({
+				rescaleOnResize: true,
+				align: 'center'
+			});
+		}, 500);
+	});
+	
 	slider.on('init', function() {
 		setTimeout(function() {
 			$('.img-scale').imageScale({
@@ -10,6 +20,20 @@ $(document).ready(function () {
 		}, 500);
 	});
 
+	mainSlider.slick({
+		autoplay: true,
+        dots: false,
+        infinite: true,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        adaptiveHeight: true,
+        adaptiveWidth: true,
+        fade: true,
+        cssEase: 'linear'
+	});
 	slider.slick({
 		autoplay: true,
         dots: false,
